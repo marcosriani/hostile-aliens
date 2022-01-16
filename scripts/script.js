3a099e2bdf875ccec4b58b842bee1ea18a172e90
Marcos Riani <marcosriani@hotmail.com>
2022-01-16 20:08:45 +0000

    Game working with possibility of restarting the game.

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,19 @@
 const shipSky = document.querySelector('.ship-sky');
 
 class GameWorld {
-  constructor(sky) {
+  constructor(sky, gameOverWrapper, buttonFight) {
     this.sky = sky;
+    this.gameOverWrapper = gameOverWrapper;
+    this.buttonFight = buttonFight;
     this.allShips = [];
+    this.gameOver = false;
   }
 
   motherShip = () => {
     this.allShips.push({
-      totalPoints: 100,
+      totalPoints: 10,
       loses: 9,
       isQueen: true,
-      isDefence: false,
-      isAttack: false,
       image: '../images/queen.png',
       gotShot: false,
     });
@@ -20,13 +21,11 @@ class GameWorld {
 
   defenseShips = () => {
     //Generate 5 defence ships
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 2; i++) {
       this.allShips.push({
         totalPoints: 80,
         loses: 10,
         isQueen: false,
-        isDefence: true,
-        isAttack: false,
         image: '../images/defence.png',
         gotShot: false,
       });
@@ -35,13 +34,11 @@ class GameWorld {
 
   attackShips = () => {
     //Generate 5 defence ships
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 2; i++) {
       this.allShips.push({
         totalPoints: 45,
         loses: 12,
         isQueen: false,
-        isDefence: false,
-        isAttack: true,
         image: '../images/attack.png',
         gotShot: false,
       });
@@ -56,39 +53,42 @@ class GameWorld {
   };
 
   attackedShip = () => {
-    // deep copy of objects inside the array
-    const allShipsCopy = this.allShips.map((obj) => {
-      const newObject = { ...obj };
-      newObject.gotShot = false;
-      return newObject;
-    });
+    if (this.allShips.length > 0 && !this.gameOver) {
+      // deep copy of objects inside the array
+      const allShipsCopy = this.allShips.map((obj) => {
+        const newObject = { ...obj };
+        newObject.gotShot = false;
+        return newObject;
+      });
 
-    const randomShip =
-      allShipsCopy[this.getRandomIntInclusive(0, allShipsCopy.length - 1)];
+      const randomShip =
+        allShipsCopy[this.getRandomIntInclusive(0, allShipsCopy.length - 1)];
 
-    randomShip.totalPoints -= randomShip.loses;
-    randomShip.gotShot = true;
+      randomShip.totalPoints -= randomShip.loses;
+      randomShip.gotShot = true;
 
-    if (randomShip.totalPoints <= 0) {
-      randomShip.image = '../images/explosion.png';
-      randomShip.totalPoints = 0;
-
-      const index = allShipsCopy.indexOf(randomShip);
-      if (index > -1) {
-        setTimeout(() => {
-          allShipsCopy.splice(index, 1);
-          this.allShips = allShipsCopy;
-        }, 100);
+      if (randomShip.totalPoints <= 0) {
+        randomShip.image = '../images/explosion.png';
+        randomShip.totalPoints = 0;
+        const index = allShipsCopy.indexOf(randomShip);
+        if (index > -1 && !this.gameOver) {
+          setTimeout(() => {
+            allShipsCopy.splice(index, 1);
+            this.allShips = allShipsCopy;
+            if (randomShip.isQueen && randomShip.totalPoints <= 0) {
+              this.gameOver = true;
+              if (this.gameOver) {
+                this.gameOverWrapper.classList.remove('game-over-wrapper');
+                this.buttonFight.classList.add('game-over-wrapper');
+              }
+            }
+          }, 100);
+        }
+        this.allShips = allShipsCopy;
       }
-      this.allShips = allShipsCopy;
+
+      if (randomShip.totalPoints > 0) this.allShips = allShipsCopy;
     }
-    console.log(randomShip.gotShot);
-
-    if (randomShip.totalPoints > 0) this.allShips = allShipsCopy;
-
-    console.log(randomShip);
-    // console.log(this.allShips);
-    console.log(allShipsCopy);
   };
 
   // Generate the ships on the sky
@@ -113,15 +113,31 @@ class GameWorld {
       this.sky.appendChild(div);
     });
   };
+
+  startAgain = () => {
+    // this.window.location.reload();
+    // return false;
+    this.gameOver = false;
+    this.gameOverWrapper.classList.add('game-over-wrapper');
+    this.buttonFight.classList.remove('game-over-wrapper');
+    this.allShips = [];
+    this.gameGenerator();
+  };
 }
 
 const buttonFight = document.querySelector('.controller__button');
+const gameOverWrapper = document.querySelector('.game-over-wrapper');
+const startAgain = document.querySelector('.game-over__continue');
 
-const initiateWorld = new GameWorld(shipSky);
+const initiateWorld = new GameWorld(shipSky, gameOverWrapper, buttonFight);
 
 initiateWorld.gameGenerator();
 
 buttonFight.addEventListener('click', () => {
   initiateWorld.attackedShip();
   initiateWorld.gameGenerator();
+});
+
+startAgain.addEventListener('click', () => {
+  initiateWorld.startAgain();
 });
